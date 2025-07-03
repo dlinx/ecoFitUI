b6dda9ead92e411acbdd348652fc3de2d42db044
Laxmikant Dange <5583697+dlinx@users.noreply.github.com>
2025-07-03 16:17:03 +0530

    test ci

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 // import App from './App';
 import './index.css';
+import { Typography } from '@mui/material';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    Welcome to EcoFit. This site is under development. Please check back later.
+    <Typography variant="h1">Welcome to EcoFit.</Typography>
+    <Typography variant="body1">This site is under development. Please check back later.</Typography>
     {/* <App /> */}
   </StrictMode>
 );
